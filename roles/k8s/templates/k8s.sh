2f46fe24e4eb09fcefe3195cef9fbc3b5c9c6c44
zhouxingcheng <cq_xxlaila@163.com>
2018-11-27 15:34:31 +0800

    更新内容

--- roles/k8s/templates/k8s.sh
+++ roles/k8s/templates/k8s.sh
@@ -1,5 +1,7 @@
- #!/bin/bash
-images=(kube-proxy-amd64 kube-scheduler-amd64 kube-controller-manager-amd64 kube-apiserver-amd64 etcd coredns pause-amd64 kubernetes-dashboard-amd64 kubernetes-dashboard-init-amd64 k8s-dns-sidecar-amd64 k8s-dns-kube-dns-amd64 k8s-dns-dnsmasq-nanny-amd64 heapster-amd64)
+#!/bin/bash
+images=(kube-proxy-amd64:v1.10.7 kube-scheduler-amd64:v1.10.7 kube-controller-manager-amd64:v1.11.2
+        kube-apiserver-amd64:v1.11.2 etcd:3.2.18 coredns:1.2.2 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.10.0 kubernetes-dashboard-init-amd64:v1.0.1
+        k8s-dns-sidecar-amd64:1.14.9 k8s-dns-kube-dns-amd64:1.14.9 k8s-dns-dnsmasq-nanny-amd64:1.14.9 heapster-amd64:v1.5.4)
 for imageName in ${images[@]} ; do
     docker pull xxlaila/$imageName
     docker tag xxlaila/$imageName k8s.gcr.io/$imageName
